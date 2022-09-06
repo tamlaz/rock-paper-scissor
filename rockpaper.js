6bacceb12698fd94aab34020334ea8519be7ab58
Tamas Lazar <lazartamas97@gmail.com>
2022-09-06 22:12:03 +0200

    Add variables with textcontent to display the result of each round

--- rockpaper.js
+++ rockpaper.js
@@ -7,8 +7,16 @@ const gameTools = ['rock', 'paper', 'scissor'];
 function computerChoice() {
     return gameTools[Math.floor(Math.random() * gameTools.length)];
 }
-let computerSelection = computerChoice();
-console.log(computerSelection);
+
+let gameResults = document.querySelector('.results');
+let scorePlayer = document.querySelector('.resultplayer');
+gameResults.appendChild(scorePlayer);
+
+let scoreComputer = document.querySelector('.resultcomputer');
+gameResults.appendChild(scoreComputer);
+
+const roundTie = document.createElement('div');
+roundTie.classList.add('roundTie');
 
 /// creating function to compare playerSelection and computerSelection
 function playGround(playerSelection, computerSelection) {
@@ -16,43 +24,47 @@ function playGround(playerSelection, computerSelection) {
     /// If it is a draw, none of them got a points, it goes to the draw variable 
     if (playerSelection == computerSelection) {
         draw += 1;
-        console.log(`Draw! Both of you chose the same: ${playerSelection} equals ${computerSelection}.`);
+        roundTie.textContent = (`Tie! ${playerSelection} equals ${computerSelection}. Number of draws: ${draw}`);
+        gameResults.appendChild(roundTie);
     /// If the player wins, variable playerWin is increased with 1
     } else if (playerSelection == 'rock' && computerSelection == 'scissor') {
-            playerWin += 1;
-            console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+        playerWin += 1;
+        scorePlayer.textContent = "Your score : " + playerWin;
     } else if (playerSelection == 'scissor' && computerSelection == 'paper') {
-            playerWin += 1;
-            console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+        playerWin += 1;
+        scorePlayer.textContent = "Your score : " + playerWin;
+        gameResults.appendChild(scorePlayer);
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-            playerWin += 1;
-            console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+        playerWin += 1;
+        scorePlayer.textContent = "Your score : " + playerWin;
+        gameResults.appendChild(scorePlayer);
     /// If the computer wins, the variable computerWin is increased with 1
     } else {
         computerWin += 1;
-        console.log(`The Computer won! ${computerSelection} beats ${playerSelection}`);
+        scoreComputer.textContent = "Computer score: " + computerWin;
+        
     }
-    console.log(`You have: ${playerWin} point(s).`);
-    console.log(`The Computer has: ${computerWin} point(s).`);
-    console.log(`Number of draws: ${draw}.`);
+    // console.log(`You have: ${playerWin} point(s).`);
+    // console.log(`The Computer has: ${computerWin} point(s).`);
+    // console.log(`Number of draws: ${draw}.`);
     /// Used if...else statements to determine who won the entire game
     // if (computerWin == 5) {
     // console.log('The Computer won the game!');
     // } else if (playerWin == 5) {
     // console.log('Congratulation, you won!');
     // }
-    }
+}
 
 let buttons = document.querySelectorAll('.buttons');
 
 for (i of buttons) {
     i.addEventListener('click', function(e) {
         let playerSelection = e.target.value;
-        playGround(playerSelection, computerSelection)
+        let computerSelection = computerChoice();
+        console.log(computerSelection);
+        playGround(playerSelection, computerSelection);
     })
-}
-
-let currentScore = 
+};
 
 
 
