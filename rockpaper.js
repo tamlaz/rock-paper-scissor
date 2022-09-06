bee981b4ee291cd420cf5de885048285b0605d03
Tamas Lazar <lazartamas97@gmail.com>
2022-09-06 12:00:59 +0200

    Add event listener to buttons

---
+++ rockpaper.js
@@ -0,0 +1,45 @@
+/// creating variables with values of 0, to later counting the score
+let playerWin = 0;
+let computerWin = 0;
+let draw = 0;
+
+const gameTools = ['rock', 'paper', 'scissor'];
+function computerChoice() {
+    return gameTools[Math.floor(Math.random() * gameTools.length)];
+}
+let computerSelection = computerChoice();
+console.log(computerSelection);
+
+/// creating function to compare playerSelection and computerSelection
+function playGround(playerSelection, computerSelection) {
+    /// If...else statements for every case, both of the computer and player can select rock, paper, scissor
+    /// If it is a draw, none of them got a points, it goes to the draw variable 
+    if (playerSelection == computerSelection) {
+        draw += 1;
+        console.log(`Draw! Both of you chose the same: ${playerSelection} equals ${computerSelection}.`);
+    /// If the player wins, variable playerWin is increased with 1
+    } else if (playerSelection == 'rock' && computerSelection == 'scissor') {
+            playerWin += 1;
+            console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+    } else if (playerSelection == 'scissor' && computerSelection == 'paper') {
+            playerWin += 1;
+            console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+            playerWin += 1;
+            console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+    /// If the computer wins, the variable computerWin is increased with 1
+    } else {
+        computerWin += 1;
+        console.log(`The Computer won! ${computerSelection} beats ${playerSelection}`);
+    }
+}
+
+let buttons = document.querySelectorAll('.buttons');
+
+for (i of buttons) {
+    i.addEventListener('click', function(e) {
+        let playerSelection = e.target.value;
+        playGround(playerSelection, computerSelection)
+    })
+}
+
